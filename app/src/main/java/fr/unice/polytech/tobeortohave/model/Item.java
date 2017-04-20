1aba0a4bade82d1e473c9421aec15a27c6edf680
Alexandre Clement <alexandre.clement@etu.unice.fr>
2017-04-20 21:55:28 +0200

    Ajout du model.

---
+++ app/src/main/java/fr/unice/polytech/tobeortohave/model/Item.java
@@ -0,0 +1,58 @@
+package fr.unice.polytech.tobeortohave.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alexandre Clement
+ *         Created the 20/04/2017.
+ */
+
+public class Item
+{
+    public static enum Category
+    {
+        BOOK,
+        DVD;
+    }
+
+    private final int id;
+    private final String name;
+    private final Category category;
+    private final double price;
+    private final String description;
+
+    public Item(int id, String name, Category category, double price, String description)
+    {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Category getCategory()
+    {
+        return category;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+}
